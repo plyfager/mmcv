8bcf7fdf028e0cb421deff9275b7288d664cde13
seuyou <3463423099@qq.com>
2022-02-15 15:51:18 +0800

    [Fix]: import ToTensor in transforms __init__

--- mmcv/transforms/__init__.py
+++ mmcv/transforms/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import TRANSFORMS
 from .wrappers import ApplyToMultiple, Compose, RandomChoice, Remap
+from .formatting import ToTensor
 
-__all__ = ['TRANSFORMS', 'ApplyToMultiple', 'Compose', 'RandomChoice', 'Remap']
+__all__ = [
+    'TRANSFORMS', 'ApplyToMultiple', 'Compose', 'RandomChoice', 'Remap',
+    'ToTensor'
+]
