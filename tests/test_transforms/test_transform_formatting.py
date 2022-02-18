912c1b627b433bee731018b6e57e7d1692b68800
seuyou <3463423099@qq.com>
2022-02-18 20:22:31 +0800

    [Feature]: Add comments to the UT of ToTensor and ImageToTensor

--- tests/test_transforms/test_transform_formatting.py
+++ tests/test_transforms/test_transform_formatting.py
@@ -10,26 +10,32 @@ from mmcv.transforms import (ImageToTensor, RandomFlip, RandomResize, ToTensor,
 
 def test_to_tensor():
 
+    # The type of the input object is torch.Tensor
     data_tensor = torch.tensor([1, 2, 3])
     tensor_from_tensor = to_tensor(data_tensor)
     assert isinstance(tensor_from_tensor, torch.Tensor)
 
+    # The type of the input object is numpy.ndarray
     data_numpy = np.array([1, 2, 3])
     tensor_from_numpy = to_tensor(data_numpy)
     assert isinstance(tensor_from_numpy, torch.Tensor)
 
+    # The type of the input object is list
     data_list = [1, 2, 3]
     tensor_from_list = to_tensor(data_list)
     assert isinstance(tensor_from_list, torch.Tensor)
 
+    # The type of the input object is int
     data_int = 1
     tensor_from_int = to_tensor(data_int)
     assert isinstance(tensor_from_int, torch.Tensor)
 
+    # The type of the input object is float
     data_float = 1.0
     tensor_from_float = to_tensor(data_float)
     assert isinstance(tensor_from_float, torch.Tensor)
 
+    # The type of the input object is invalid
     with pytest.raises(TypeError):
         data_str = '123'
         _ = to_tensor(data_str)
@@ -41,30 +47,20 @@ class TestToTensor:
         TRANSFORM = ToTensor(keys=['img_label'])
         assert TRANSFORM.keys == ['img_label']
 
-    def test_fetch_data(self):
-        TRANSFORM = ToTensor(['img_label'])
-        results = {'img_label': [1]}
-        fetched_data = TRANSFORM._fetch_data(results, 'img_label')
-        assert isinstance(fetched_data, list)
-
-        TRANSFORM = ToTensor(['instances.bbox'])
-        results = {'instances': {'bbox': [0, 0, 1, 1]}}
-        fetched_data = TRANSFORM._fetch_data(results, 'instances.bbox')
-        assert isinstance(fetched_data, list)
-
-        TRANSFORM = ToTensor(['instances.label'])
-        results = {'instances': {'bbox': [0, 0, 1, 1]}}
-        fetched_data = TRANSFORM._fetch_data(results, 'instances.label')
-        assert fetched_data is None
-        fetched_data = TRANSFORM._fetch_data(results, 'object.label')
-        assert fetched_data is None
-
     def test_transform(self):
-        TRANSFORMS = ToTensor(['instances.bbox', 'img_label'])
+        TRANSFORMS = ToTensor(['instances.bbox', 'img_label', 'bbox'])
+
+        # Test multi-level key and single-level key (multi-level key is
+        # not in results)
         results = {'instances': {'label': [1]}, 'img_label': [1]}
-        results_tensor = TRANSFORMS.transform(results)
+        results_tensor = TRANSFORMS.transform(copy.deepcopy(results))
         assert isinstance(results_tensor['instances']['label'], list)
         assert isinstance(results_tensor['img_label'], torch.Tensor)
+
+        # Test multi-level key (multi-level key is in results)
+        results = {'instances': {'bbox': [1]}, 'img_label': [1]}
+        results_tensor = TRANSFORMS.transform(copy.deepcopy(results))
+        assert isinstance(results_tensor['instances']['bbox'], torch.Tensor)
 
     def test_repr(self):
         TRANSFORMS = ToTensor(['instances.bbox', 'img_label'])
@@ -80,10 +76,13 @@ class TestImageToTensor:
 
     def test_transform(self):
         TRANSFORMS = ImageToTensor(['img'])
+
+        # image only has one channel
         results = {'img': np.zeros((224, 224))}
         results = TRANSFORMS.transform(results)
         assert results['img'].shape == (1, 224, 224)
 
+        # image has three channels
         results = {'img': np.zeros((224, 224, 3))}
         results = TRANSFORMS.transform(results)
         assert results['img'].shape == (3, 224, 224)
