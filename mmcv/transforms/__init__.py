b65973f50f4f15dd60da291e6f24f7706bf6aa4f
seuyou <3463423099@qq.com>
2022-02-21 11:19:14 +0800

    [Fix]: move RandomFlip and RandomResize to processing.py

--- mmcv/transforms/__init__.py
+++ mmcv/transforms/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import TRANSFORMS
-from .formatting import (ImageToTensor, RandomFlip, RandomResize, ToTensor,
-                         to_tensor)
+from .formatting import ImageToTensor, ToTensor, to_tensor
+from .processing import RandomFlip, RandomResize
 from .wrappers import ApplyToMultiple, Compose, RandomChoice, Remap
 
 __all__ = [
