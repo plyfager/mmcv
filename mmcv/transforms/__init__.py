d2c59bec98b77af906ea2a8595ea76eb40fc21f7
seuyou <3463423099@qq.com>
2022-02-21 17:29:42 +0800

    [Fix]: Fix no torch bug

--- mmcv/transforms/__init__.py
+++ mmcv/transforms/__init__.py
@@ -1,10 +1,21 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .builder import TRANSFORMS
-from .formatting import ImageToTensor, ToTensor, to_tensor
-from .processing import RandomFlip, RandomResize
-from .wrappers import ApplyToMultiple, Compose, RandomChoice, Remap
 
-__all__ = [
-    'TRANSFORMS', 'ApplyToMultiple', 'Compose', 'RandomChoice', 'Remap',
-    'ToTensor', 'to_tensor', 'ImageToTensor', 'RandomFlip', 'RandomResize'
-]
+try:
+    import torch  # noqa: F401
+except ImportError:
+    from .processing import RandomFlip, RandomResize
+    from .wrappers import ApplyToMultiple, Compose, RandomChoice, Remap
+    __all__ = [
+        'TRANSFORMS', 'ApplyToMultiple', 'Compose', 'RandomChoice', 'Remap',
+        'RandomFlip', 'RandomResize'
+    ]
+else:
+    from .formatting import ImageToTensor, ToTensor, to_tensor
+    from .processing import RandomFlip, RandomResize
+    from .wrappers import ApplyToMultiple, Compose, RandomChoice, Remap
+
+    __all__ = [
+        'TRANSFORMS', 'ApplyToMultiple', 'Compose', 'RandomChoice', 'Remap',
+        'ToTensor', 'to_tensor', 'ImageToTensor', 'RandomFlip', 'RandomResize'
+    ]
